2fea7953e5e8cb14e8dcc89d897e7c4ba6e58448
Lucas Alexandre <lucasalexandre.lads13@gmail.com>
2026-02-27 00:11:07 -0300

    feat: adiciona funções de cálculo e classificação de IMC

--- modulo/calculos/calculo-imc.js
+++ modulo/calculos/calculo-imc.js
@@ -6,3 +6,61 @@
 * Versão: 1.0
 
 ****************************************************************************/
+
+const validacao = require('../validacaoDados.js')
+
+const calculoDoImc = function(peso, altura){
+    if( !validacao.validarVazio(peso)          || !validacao.validarVazio(altura)  || 
+        !validacao.validarNumero(peso)         || !validacao.validarNumero(altura) ||
+        !validacao.validarNumeroPositivo(peso) || !validacao.validarNumeroPositivo(altura)){
+
+        return false    
+    }        
+    
+    let valorPeso   = Number(peso)
+    let valorAltura = Number(altura)
+    let resultadoImc
+
+    resultadoImc = valorPeso / (valorAltura * valorAltura) 
+    
+    return resultadoImc
+}
+
+const calculoTabelaClassificacao = function(peso, altura){
+    const resultadoImc = calculoDoImc(peso, altura)
+    
+    if(!resultadoImc){
+        return false
+    }
+
+    let classificacaoPeso
+
+    if(resultadoImc < 18.5){
+        classificacaoPeso = 'Abaixo do peso'
+
+    }else if(resultadoImc <= 24.9){
+        classificacaoPeso = 'Peso normal'
+
+    }else if(resultadoImc <= 29.9){
+        classificacaoPeso = 'Acima do peso (sobrepeso)'
+    
+    }else if(resultadoImc <= 34.9){
+        classificacaoPeso = 'Obesidade I'
+    
+    }else if(resultadoImc <= 39.9){
+        classificacaoPeso = 'Obesidade II'
+
+    }else if(resultadoImc >= 40){
+        classificacaoPeso = 'Obesidade III'
+
+    }else{
+        return false
+    }
+
+    return classificacaoPeso
+}
+
+module.exports ={
+    calculoDoImc,
+    calculoTabelaClassificacao
+}
